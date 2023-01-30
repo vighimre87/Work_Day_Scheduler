7b68c0d68b63974e21c2fd0f664a55f743ac533c
Imre Vigh <vighimre87@gmail.com>
2023-01-30 20:57:32 +0000

    made function coloring timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,59 @@
 // Target the areas that we are interacting with in HTML
 const currentDay = $("#currentDay");
+currentDay.text(moment().format("dddd, MMMM Do YYYY"));
 const timeblocks = $("#timeblocks");
-const officeHoursOfDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-const currentTime = moment().calendar();
+const officeHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const currentHour = eval(moment().format("HH"));
+const textAreas = $(".description");
 
 
-// Display the current date and day
-setInterval(function displayDay() {
-    let date = moment();
-    currentDay.text(date.format("dddd, MMMM Do YYYY"));
-}, 1000);
+$(".hour").each(function() {
+    let hour = $(this).text().split(":");
+    if(currentHour === eval(hour[0])) {
+        $(this).parent().addClass("present");
+    } else {
+        if (currentHour < eval(hour[0])) {
+        $(this).parent().addClass("future");
+        $(this).parent().removeClass("present");
+        } else {
+            $(this).parent().addClass("past");
+            $(this).parent().removeClass("future");
+        }
+    }
+
+})
+
+// function changeColor(time) {
+//     for(let i = 0; i<officeHours.length;i++) {
+//         if ()
+//     }
+// }
+
+
+
+
 
 // Dynamically generating the timeblocks as the user opens the website
 // function generateTimeBlocks() {
 //     // Looping through the officeHoursOfDay to create the layout and the content of the timeblocks
-//     for (let i= 0; i<officeHoursOfDay.length; i++) {
-//         const timeblockForm = $("<form data-content=officeHours[i] class='time-block'></form>");
-//         timeblocks.append(timeblockForm);
-//         const officeHour = $("<div class='hour'></div>");
-//         officeHour.text = officeHoursOfDay[i];
-//         timeblockForm.append(officeHour);
+//     const timeblockTable = $("<table class='row'></table>");
+//     timeblocks.append(timeblockTable);
+//     for (let i= 0; i<officeHours.length; i++) {
+//         const timeblock = $("<tr class='time-block'></tr>");
+//         timeblockTable.append(timeblock);
+//         const officeHour = $("<td class='hour'></td>");
+//         officeHour.text = officeHours[i];
+//         console.log(officeHour.text);
+//         timeblockTable.append(officeHour);
+//         const userEventContainer = $("<td>");
+//         userEventContainer.append(timeblock);
 //         const userEvent = $("<textarea class='description'></textarea>");
 //         // userEvent.text = localStorage.getItem();
-//         timeblockForm.append(userEvent);
-//         const saveBtn = $("<button type='submit' class='saveBtn'><i class='fa-solid fa-floppy-disk'></i></button>");
-//         timeblockForm.append(saveBtn);
+//         userEventContainer.append(userEvent);
+//         const saveBtnContainer = $("<td>");
+//         timeblock.append(saveBtnContainer);
+//         const saveBtn = $("<button type='submit' class='saveBtn'></button>");
+//         saveBtn.text = "Submit";
+//         saveBtnContainer.append(saveBtn);
 //     };
 // }
-
-
-// generateTimeBlocks();
